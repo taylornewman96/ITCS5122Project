7af5e8123c6a36c07b577a5b41be1b9458fa866b
Sakthivel Ravichandran <sakthivelravichandran18@gmail.com>
2022-04-20 21:11:14 -0400

    Added title and sidebar

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pickle
 
-st.title('Insights for Data Science roles in US')
+def load_clf_model():
+    filename = "finalized_model_clf.sav"
+    loaded_model = pickle.load(open(filename, 'rb'))
+    return loaded_model
+
+
+st.write("""Insights for Data Science roles in US""")
+
+st.markdown("""
+This app predicts <needs to be added>
+* **Python libraries:** streamlit
+* **Data source:** [Data-scientist-salary](https://www.kaggle.com/datasets/sulianova/cardiovascular-disease-dataset).
+""")
+
+st.sidebar.write("""
+### User Input Features
+""")    
+
