6d6394837850fd3007944aebab9a471f508c45f2
Sakthivel Ravichandran <sakthivelravichandran18@gmail.com>
2022-04-26 22:35:56 -0400

    added charts

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from tkinter import font
+from tokenize import group
 import streamlit as st
 import altair as alt
 import seaborn as sns
+import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import pickle
+from vega_datasets import data
+
+matplotlib.use("Qt4Agg")
+
 
 def load_clf_model():
     filename = "finalized_model_clf.sav"
@@ -134,8 +140,25 @@ if predict_btn:
     if pred and len(pred) > 0:
         st.sidebar.write("Prediction result: You can expect a salary of $" + str(round(pred[0], 2))+ "k")
 
-df = pd.DataFrame(load_chart_data())
 
+df = pd.DataFrame(load_chart_data())
+print(df.head(5))
+#map
+states = alt.topo_feature(data.us_10m.url, 'states')
+
+map = alt.Chart(states).mark_geoshape().encode().transform_lookup(
+    lookup='index',
+    from_=alt.LookupData(df, 'index', list(df.columns))
+).project(
+    type='albersUsa'
+).properties(
+    width=1000,
+    height=500
+)
+st.write(map)
+
+col1, col2 =  st.columns(2)
+# revenue chart
 df = df[df.Revenue != 'Unknown / Non-Applicable']
 
 revenue_chart = alt.Chart(df, title = 'Number of Companies Per Revenue Bracket').mark_bar().encode(
@@ -143,26 +166,76 @@ revenue_chart = alt.Chart(df, title = 'Number of Companies Per Revenue Bracket')
     alt.Y('count()', title = 'Company Count'),
     tooltip=['count():O']
 ).configure_axis(
-    titleFontSize=8
+    titleFontSize=12
 ).configure_title(
-    fontSize=12
+    fontSize=15
 ).properties(
-     width=450,
-    height=350
+     width=500,
+    height=500
 ).interactive()
-
+col1.write(revenue_chart)
 # st.write(revenue_chart)
 
+# Industries With The Most Available Jobs - pie chart
 df_industry = df['Industry'].value_counts()
 top_5 = df_industry[0:5]
-
 fig1, ax1 = plt.subplots()
 ax1.pie(top_5, labels=top_5.index, autopct='%1.1f%%',)
 # ax1.title.set_text('Industries With The Most Available Jobs')
-
 # st.pyplot(fig1)
+plt.title('\n Industries With The Most Available Jobs  \n', size=10, color='black')
+# col2.write(""" ###### Industries With The Most Available Jobs  """)
+col1.pyplot(fig1)
 
-col1, col2 =  st.columns(2)
-col1.write(revenue_chart)
-col2.write(""" ###### Industries With The Most Available Jobs  """)
-col2.pyplot(fig1)
+top10_max_job_posting_df = pd.DataFrame({
+    'Company name': df["company_txt"].value_counts().sort_values(ascending=False).head(10).index,
+    'Company count': df["company_txt"].value_counts().sort_values(ascending=False).head(10)
+}) 
+
+# Top 10 Companies with Maximum Number of Job Postings
+top10_max_job_posting_chart = alt.Chart(top10_max_job_posting_df, title = 'Top 10 Companies with Maximum Number of Job Postings').mark_bar().encode(
+    alt.X('Company name'),
+    alt.Y('Company count', title = 'Job Posting Count'),
+    tooltip=['Company count'],
+    color=alt.Color('Company name', scale=alt.Scale(scheme='dark2'), legend=None)
+).configure_axis(
+    titleFontSize=12
+).configure_title(
+    fontSize=15
+).properties(
+     width=500,
+    height=500
+).interactive()
+col2.write(top10_max_job_posting_chart)
+
+# Top 10 states with avg annual minimal and maximal salaries
+sort_ind = df["Location"].value_counts().sort_values(ascending=False).index
+ind = df.groupby("Location")["Lower Salary","Upper Salary"].mean().sort_values("Location",ascending=False)
+ind = ind.reset_index()
+ind["Location"] = ind["Location"].astype("category")
+ind["Location"].cat.set_categories(sort_ind, inplace=True)
+ind = ind.sort_values(["Location"]).reset_index()
+ind = ind.drop("index",axis=1)
+ind.head(2)
+lab=[]
+for i in sort_ind[0:10]:
+  lab.append(i)
+x = np.arange(len(lab))
+width = 0.35
+fig2, ax2 = plt.subplots()
+rects1 = ax2.bar(x - width/2, ind["Lower Salary"][0:10], width, label='Min avg Salary')
+rects2 = ax2.bar(x + width/2, ind["Upper Salary"][0:10], width, label='Max avg Salary')
+plt.title('\n Top 10 locations with Average Annual Minimal and Maximal Salaries \n', size=10, color='black')
+plt.xticks(fontsize=8)
+plt.xticks(rotation=75,ha='right', rotation_mode='anchor')
+plt.yticks(fontsize=8)
+plt.xlabel('\n Locations \n', fontsize=8, color='black')
+plt.ylabel('\n Salary (K) \n', fontsize=8, color='black')
+fig2.tight_layout()
+ax2.set_xticks(x)
+ax2.set_xticklabels(lab)
+ax2.legend(loc="upper right")
+# fig2.set_figwidth(6)
+# fig2.set_figheight(4)
+
+col2.write(fig2)
